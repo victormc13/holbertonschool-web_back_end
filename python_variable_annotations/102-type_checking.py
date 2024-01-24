d638b1e77b5947da0b37fbbf36eac73fcc3cc494
victormc13 <viktorsb14@gmail.com>
2024-01-23 23:50:17 -0800

    new: UseS mypy to validate the following piece of code and apply any necessary changes

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Defines 'zoom_array' function that receives a tuple and int
+and returns a list type
+"""
+
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List: 
+    """Returns a list"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+tuple_int = (12, 72, 91)
+
+zoom_2x = zoom_array(tuple_int)
+
+zoom_3x = zoom_array(tuple_int, 3)
