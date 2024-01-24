acce981c5faf055a0a3055e7d348a98716222625
victormc13 <viktorsb14@gmail.com>
2024-01-23 20:58:49 -0800

    new: Creates 'sum_list' function to sum all elements of a list of floats and be returned as float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Script that defines 'sum_list' function which takes a list of floats
+and returns the sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Calculate the sum of a list of floats"""
+    for num in input_list:
+        result += num
+        
+    return float(result)
