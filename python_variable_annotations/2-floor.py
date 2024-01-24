34ed3aff4da618cec6974b8faff488a7ba377ea4
victormc13 <viktorsb14@gmail.com>
2024-01-23 19:51:57 -0800

    new: Creates the 'floor' function that returns the floor of a float number

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Script that defines 'floor' function which takes a float
+and returns the floor of the float.
+"""
+
+from math import floor
+
+
+def concat(n: float) -> int:
+    """Return the floor of a float"""
+    return floor(n)
