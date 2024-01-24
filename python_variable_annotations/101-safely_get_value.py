9240027920af123a181d571f766bf15b7e3fabe7
victormc13 <viktorsb14@gmail.com>
2024-01-23 23:07:43 -0800

    new: Defines 'safely_get_value' function that takes 3 arguments and returns 'T' type variable or Any type variable

---
+++ python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Defines 'safely_get_value' function that takes 3 arguments
+and returns 'T' type variable or Any type variable
+"""
+
+from typing import Mapping, TypeVar, Union, Any
+
+# Define a generic type variable T
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Union[Any], default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Safely get a value from a dictionary.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
+
