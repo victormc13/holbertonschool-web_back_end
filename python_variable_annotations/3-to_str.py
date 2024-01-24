484197757dbe5af4aa68714d871b1603bc9d9a97
victormc13 <viktorsb14@gmail.com>
2024-01-23 20:01:27 -0800

    new: Creates the 'to_str' function to parse a float number to string, declaring variables using type annotations

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script that defines 'to_str' function which takes a float
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float"""
+    return str(n)
