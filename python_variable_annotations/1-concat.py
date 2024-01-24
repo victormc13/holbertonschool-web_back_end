f0b9480fd3b305e8bfc138d883ea0cd311a32ccd
victormc13 <viktorsb14@gmail.com>
2024-01-23 19:42:27 -0800

    new: Creates 'concat' function that concatenate two strings and returns concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script that define 'concat' function that takes two strings
+and returns concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result"""
+    return str1 + str2
