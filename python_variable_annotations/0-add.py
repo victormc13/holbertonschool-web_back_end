4d5758f31a3453a75e077b10394911884b720b34
victormc13 <viktorsb14@gmail.com>
2024-01-23 18:20:34 -0800

    new: Creates 'add' function using type annotations for it's parameters

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Script that define 'add' function that takes two float numbers
+and returns their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the result"""
+    return a + b
