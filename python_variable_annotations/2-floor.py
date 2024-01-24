b27caf0aa398f3bb3e42619e68494bbb42f9f2e8
victormc13 <viktorsb14@gmail.com>
2024-01-23 19:57:12 -0800

    update: Updates imported 'math' module to be imported globally the 'math' module

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -5,9 +5,9 @@ Script that defines 'floor' function which takes a float
 and returns the floor of the float.
 """
 
-from math import floor
+import math
 
 
 def floor(n: float) -> int:
     """Return the floor of a float"""
-    return floor(n)
+    return math.floor(n)
