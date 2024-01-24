18b99e3d9765d6769f46afd0179cde82632fed72
victormc13 <viktorsb14@gmail.com>
2024-01-23 21:02:26 -0800

    update: Updates the function using 'typing' module for annotations

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,9 +5,12 @@ Script that defines 'sum_list' function which takes a list of floats
 and returns the sum as a float.
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Calculate the sum of a list of floats"""
+    result = 0
     for num in input_list:
         result += num
         
