961ab0eab19600efdbbdd517c752765496bea894
victormc13 <viktorsb14@gmail.com>
2024-01-29 07:03:14 -0800

    new: Creates an asynchronous coroutine 'wait_random' that waits for a random delay between 0 and max_delay seconds.

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Creates a coroutine that use the random module"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine that waits for a random delay
+    between 0 and max_delay seconds."""
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
