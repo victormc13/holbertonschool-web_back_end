3d837a5ba5c2fd688b1bf7630f00aaf4367b7a3f
victormc13 <viktorsb14@gmail.com>
2024-01-29 07:27:05 -0800

    new: Creates a async coroutine that use the 0-basic_async_syntax module

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -15,5 +15,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = await asyncio.gather(*tasks)
+
+    delays.extend(results)
 
     return sorted(delays)
