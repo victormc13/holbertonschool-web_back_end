8823608a47c1b0859a2d99c11386d07d98cd8c5d
victormc13 <viktorsb14@gmail.com>
2024-01-29 08:09:37 -0800

    update: use Generator annotation instead of AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that not takes arguments"""
     for _ in range(10):
         await asyncio.sleep(1)
