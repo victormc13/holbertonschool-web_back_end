d2bba79ce7cbd0c3312c22e2933ffbecba2362a5
victormc13 <viktorsb14@gmail.com>
2024-01-31 20:16:13 -0800

    update: Fix pycodestyle coding style

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,5 +8,6 @@ def insert_school(mongo_collection, **kwargs):
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
 
+
 if __name__ == '__main__':
     insert_school(mongo_collection, **kwargs)
