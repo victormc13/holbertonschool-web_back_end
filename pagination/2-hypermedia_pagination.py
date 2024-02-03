9f7c705d308e8016995a96dbc220f3c9b2d247c8
victormc13 <viktorsb14@gmail.com>
2024-02-02 22:28:36 -0800

    update: Fix typo error for the 'total_pages' variable

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -59,7 +59,7 @@ class Server:
         """
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
-        next_page = page + 1 if page < total_page else None
+        next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
         return {
