f9db9e23bcee70900e612891c2552f97cdd1e264
victormc13 <viktorsb14@gmail.com>
2024-02-09 15:51:31 -0500

    update: Updates the code using map function to create ClassRoom instances

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,7 @@
 import ClassRoom from "./0-classroom.js";
 
-function initializeRooms(a, b, c) {
-  const obj1 = new ClassRoom(a);
-  const obj2 = new ClassRoom(b);
-  const obj3 = new ClassRoom(c);
-  return [obj1, obj2, obj3];
+export default function initializeRooms() {
+  const roomSizes = [19, 20, 34]
+  const rooms = roomSizes.map(size => new ClassRoom(size))
+  return rooms;
 }
-
-initializeRooms(19, 20, 34);
