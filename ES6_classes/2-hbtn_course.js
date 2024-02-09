72119daaaac57ed5c9c5654f39b5b486e569f3f3
victormc13 <viktorsb14@gmail.com>
2024-02-09 18:33:53 -0500

    update: Fix linter errors for validation functions

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -29,21 +29,21 @@ export default class HolbertonCourse {
     this._students = this.validateStudents(value);
   }
 
-  validateName(name) {
+  static validateName(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     return name;
   }
 
-  validateLength(length) {
+  static validateLength(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     return length;
   }
 
-  validateStudents(students) {
+  static validateStudents(students) {
     if (
       !Array.isArray(students) ||
       !students.every((student) => typeof student === 'string')
