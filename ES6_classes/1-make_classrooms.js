7bb9ae2008f020b62b8d48cc56e179458f46c641
victormc13 <viktorsb14@gmail.com>
2024-02-09 15:53:53 -0500

    udpate: Fix linters errors importing the module and missing semicolons

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const roomSizes = [19, 20, 34]
-  const rooms = roomSizes.map(size => new ClassRoom(size))
+  const roomSizes = [19, 20, 34];
+  const rooms = roomSizes.map((size) => new ClassRoom(size));
   return rooms;
 }
