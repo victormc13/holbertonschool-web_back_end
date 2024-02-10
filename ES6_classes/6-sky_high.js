4a1eba1474172693f4277d370f8fab1ef8a5ade6
victormc13 <viktorsb14@gmail.com>
2024-02-10 16:18:59 -0500

    new: Implements a class named SkyHighBuilding that extends from Building and override 'evacuationWarningMessage' method to return a string

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
