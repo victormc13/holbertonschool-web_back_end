3779685fb601c9e31d94ce04751a3d1fa2330069
victormc13 <viktorsb14@gmail.com>
2024-02-10 11:23:15 -0500

    update: Fix missing trailing comma  comma-dangle eslint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -25,7 +25,6 @@ export default class Building {
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error(
-      'Class extending Building must override evacuationWarningMessage'
-    );
+      'Class extending Building must override evacuationWarningMessage');
   }
 }
