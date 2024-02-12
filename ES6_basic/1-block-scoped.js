c7f22a2cc90759810681052224b8a9a24e8d36bc
Victor MC <viktorsb14@gmail.com>
2024-02-12 01:25:24 -0500

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = false;
-    const task2 = true;
+    let task = false;
+    let task2 = true;
   }
 
   return [task,
