98f621354432c266632f79ee8d148e11cf6ac48c
Victor MC <viktorsb14@gmail.com>
2024-02-12 01:28:37 -0500

    Update 1-block-scoped.js formatted

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -6,7 +6,8 @@ export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
 
-				return [task, task2];
+    return [task,
+      task2];
   }
 
   return [task,
