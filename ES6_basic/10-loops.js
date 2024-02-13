f4552d1d07ca282ad5ac5c9b53b051a3cd43a228
victormc13 <viktorsb14@gmail.com>
2024-02-13 00:10:19 -0500

    new: Rewrites the function 'appendToEachArrayValue' to use ES6’s 'for...of' operator. And replace the use of 'var' declaration

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
+  }
+
+  return newArray;
+}
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
