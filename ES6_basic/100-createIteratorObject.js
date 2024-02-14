8f82ed7fc263efde17afa44b5acc300ec1511f73
victormc13 <viktorsb14@gmail.com>
2024-02-13 23:44:35 -0500

    update: Disable 'no-prototype-builtins' lint error

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,7 @@
 export default function createIteratorObject(report) {
   function* iterateEmployees(report) {
     for (const department in report.allEmployees) {
+      // eslint-disable-next-line no-prototype-builtins
       if (report.allEmployees.hasOwnProperty(department)) {
         const employees = report.allEmployees[department];
         for (const employee of employees) {
