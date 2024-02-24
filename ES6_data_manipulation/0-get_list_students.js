fca8c13297757d25606483d6aee3465a15ed6fa1
victormc13 <viktorsb14@gmail.com>
2024-02-23 21:03:58 -0500

    new: Adds 'getListStudents' function that returns an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const guillaume = { id: 1, firstName: 'Guillaume', location: 'San Franciso' };
+  const james = { id: 2, firstName: 'James', location: 'Columbia' };
+  const serena = { id: 3, firstName: 'Serena', location: 'San Franciso' };
+
+  return [guillaume, james, serena];
+}
