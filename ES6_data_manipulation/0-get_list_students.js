f2b8493c41058166d0f5934c987f007e025b6ef4
victormc13 <viktorsb14@gmail.com>
2024-02-23 21:27:48 -0500

    update: Fix id property of 'serena' object

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
   const guillaume = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const james = { id: 2, firstName: 'James', location: 'Columbia' };
-  const serena = { id: 3, firstName: 'Serena', location: 'San Francisco' };
+  const serena = { id: 5, firstName: 'Serena', location: 'San Francisco' };
 
   return [guillaume, james, serena];
 }
