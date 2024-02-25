4d5975eb8b3bd417f4edf5c03fde7d15fefb6ede
victormc13 <viktorsb14@gmail.com>
2024-02-24 19:39:35 -0500

    update: Adds an if statement to verify the typeof 'startString' attribute

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,13 @@
 export default function cleanSet(set, startString) {
   let result = '';
-  set.forEach((value) => {
-    if (value.startsWith(startString) && startString.length > 0) {
-      result += `${value.slice(startString.length)}-`;
-    }
-  });
-  return result.slice(0, -1);
+  if (startString && typeof startString === 'string') {
+    set.forEach((value) => {
+      if (value.startsWith(startString)) {
+        result += `${value.slice(startString.length)}-`;
+      }
+    });
+
+    return result.slice(0, -1);
+  }
+  return result;
 }
